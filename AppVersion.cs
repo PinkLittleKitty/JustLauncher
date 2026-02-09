2c942c2a8439d46a04daa53c14b72308a3afc490
Santiago Fisela <santyfisela@gmail.com>
2026-02-09 16:05:55 -0300

    v1.0.4

--- AppVersion.cs
+++ AppVersion.cs
@@ -2,7 +2,7 @@ namespace JustLauncher;
 
 public static class AppVersion
 {
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
 
     public const string Name = "JustLauncher";
 
